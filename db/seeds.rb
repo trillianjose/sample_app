6eb6397c372c9775aa8407a60382bfb0a0c161cb
trillianjose <trillianjosea@gmail.com>
2019-07-03 17:56:07 +0200

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,14 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::TvShows::Simpsons.quote(5)
+  content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
